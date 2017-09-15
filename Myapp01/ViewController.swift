82450ebe2534a5d501c85f7ec7d6c29aeb685bcb
jiangzongyi <reavter103@gmail.com>
2017-09-15 14:35:02 +0800

    20170915

--- Myapp01/ViewController.swift
+++ Myapp01/ViewController.swift
@@ -10,15 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @IBOutlet weak var myLab1: UILabel!
+    
+    
+    
+    @IBAction func Click(_ sender: Any) {
+        myLab1.text = creatLottery()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func creatLottery() -> String {
+        
+        var set:Set<Int> = []
+        while set.count < 6 {
+            set.insert(Int(arc4random_uniform(49)+1))
+        }
+        return set.description
     }
+    
+    
+    
+    
+    
+    
+    
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
